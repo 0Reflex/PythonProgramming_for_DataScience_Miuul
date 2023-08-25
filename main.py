8531add36bd573716773ce23822444294d570f93
0Reflex <113729528+0Reflex@users.noreply.github.com>
2023-08-25 15:14:06 +0300

    ADVANCED_FUNCTIONAL_EDA

--- main.py
+++ main.py
@@ -969,3 +969,271 @@ plt.show()
 
 df["total_bill"].hist()
 plt.show()
+
+###############################################################################
+## ADVANCED FUNCTIONAL EDA
+###############################################################################
+
+# 1. Genel Resim
+# 2. Analysis of Categorical Variables
+# 3. Analysis of Numerical Variables
+# 4. Analysis of Target Variables
+# 5. Analysis of Correlation
+
+
+######################################
+# 1. Genel Resim
+######################################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA#####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+check_df(df)
+
+df = sns.load_dataset("flights")
+check_df(df)
+
+######################################
+# 2. Analysis of Categorical Variables
+######################################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+print(df["embarked"].value_counts())
+print(df["sex"].unique())
+print(df["class"].nunique())
+
+cat_cols = [col for col in df.columns if str(df[col].dtypes in ["category", "object", "bool"])]
+print(df[cat_cols])
+
+num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+print(num_but_cat)
+
+cat_but_car = [col for col in df.columns if df[col].nunique() > 20 and str(df[col].dtypes in ["category", "object"])]
+print(cat_but_car)
+
+cat_cols = num_but_cat + cat_cols
+print(cat_cols)
+
+cat_cols = [col for col in cat_cols if col not in cat_but_car]
+print(df[cat_cols].nunique())
+
+df["survived"].value_counts()
+100 + df["survived"].value_counts() / len(df)
+
+
+def cat_summary(dataframe, col_name):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("################################################")
+
+
+cat_summary(df, "sex")
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts()
+                                 / len(dataframe)}))
+    print("################################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+cat_summary(df, "sex", plot=True)
+
+for col in cat_cols:
+    if df[col].dtypes == "bool":
+        print("sdhfkjdfdf")
+    else:
+        cat_summary(df, col, plot=True)
+
+for col in cat_cols:
+    if df[col].dtypes == "bool":
+        df[col] = df[col].astype(int)
+    else:
+        cat_summary(df, col, plot=True)
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    if dataframe[col_name].dtypes == "bool":
+        dataframe[col_name] = dataframe[col_name].astype(int)
+        print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                            "Ratio": 100 * dataframe[col_name].value_counts()
+                                     / len(dataframe)}))
+        print("################################################")
+
+        if plot:
+            sns.countplot(x=dataframe[col_name], data=dataframe)
+            plt.show(block=True)
+    else:
+        print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                            "Ratio": 100 * dataframe[col_name].value_counts()
+                                     / len(dataframe)}))
+        print("################################################")
+
+        if plot:
+            sns.countplot(x=dataframe[col_name], data=dataframe)
+            plt.show(block=True)
+
+
+cat_summary(df, col, plot=True)
+
+######################################
+# 3. Analysis of Numerical Variables
+######################################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df[["age", "fare"]].describe().T
+
+num_cols = [col for col in df.columns if df[col].dtypes in ["int", "float"]]
+num_cols = [col for col in num_cols if col not in cat_cols]
+print(num_cols)
+
+
+def num_summary(dataframe, numerical_col, plot = False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+num_summary(df, "age", plot=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+#########################################################################
+##Capturing Variables and Generalizing Operations
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+df.info()
+
+for col in df.columns:
+    if df[col].dtypes == "bool":
+        df[col] = df[col].astype(int)
+
+#docstring
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+
+    Parameters
+    ----------
+    dataframe: dataframe
+        değişken isimleri alınmak istenen dataframe'dir.
+    cat_th: int, float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int, float
+        kategorik fakat kategorik olan değişkenler için sınıf eşik değeri
+
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişken listesi
+    num_cols: list
+        Numerik değişken listesi
+    cat_but_car: list
+        Kategorik görünümlü kordinal değişken listesi
+
+    Notes
+    ------
+    cat_?cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un içerisinde.
+
+    """
+
+    cat_cols = [col for col in df.columns if str(df[col].dtypes in ["category", "object", "bool"])]
+
+    num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int", "float"]]
+
+    cat_but_car = [col for col in df.columns if
+                   df[col].nunique() > 20 and str(df[col].dtypes in ["category", "object"])]
+
+    cat_cols = num_but_cat + cat_cols
+
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in df.columns if df[col].dtypes in ["int", "float"]]
+    num_cols = [col for col in num_cols if col not in cat_cols]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+######################################
+# 4. Analysis of Target Variables
+######################################
